b033c4ec2a4c36e9e3e3361893296efe6d3774c1
Yassine LAHMAR-CHERIF <72023637+YassineLC@users.noreply.github.com>
2023-12-13 19:38:37 +0100

    correction scripts

--- scripts/importation.sh
+++ scripts/importation.sh
@@ -5,21 +5,17 @@ PGPASSWORD="toor"
 PGHOST="localhost"
 PGDATABASE="sae"
 
+cd /home/yassine/MEGA/COURS/BUT2/SAE/scripts
 
-PGPASSWORD="$PGPASSWORD" psql -h "$PGHOST" -d "$PGDATABASE" -U "$PGUSER" -c "\i tables.sql"
+PGPASSWORD="$PGPASSWORD" psql -h "$PGHOST" -d "$PGDATABASE" -U "$PGUSER" -f "tables.sql"
 
 cd /home/yassine/MEGA/COURS/BUT2/SAE/bdd
-
-sed -i 's/Rosenkavalier""/Rosenkavalier"/' title.akas.tsv
-sed -i 's/"Thanatos Palace Hôtel/"Thanatos Palace Hôtel"/' title.principals.tsv
-sed -i 's/"Lauzun)/"Lauzun)\"/' title.principals.tsv
-sed -i -e '/tt0701219/d' ../bdd/title.basics.tsv
 
 files=(*.tsv)
 
 for file in "${files[@]}"; do
-table="${file%.tsv}"
-temp=$(echo $table | tr -d '.')
-commande="\COPY $temp FROM '$file' WITH DELIMITER E'\t' QUOTE E'\b' CSV HEADER NULL '\N';"
-PGPASSWORD="$PGPASSWORD" psql -h "$PGHOST" -d "$PGDATABASE" -U "$PGUSER" -c "$commande"
+  table="${file%.tsv}"
+  temp=$(echo "$table" | tr -d '.')
+  commande="\COPY $temp FROM '$file' WITH DELIMITER E'\t' QUOTE E'\b' CSV HEADER NULL '\N';"
+  PGPASSWORD="$PGPASSWORD" psql -h "$PGHOST" -d "$PGDATABASE" -U "$PGUSER" -c "$commande"
 done
