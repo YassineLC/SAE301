86e091b551e112d09ff487ba04d13527f3190a83
Yassine LAHMAR-CHERIF <72023637+YassineLC@users.noreply.github.com>
2024-02-05 13:53:12 +0100

    ajout de la section nouveautes

--- site/Views/view_home.php
+++ site/Views/view_home.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="Content/css/bootstrap.min.css" rel="stylesheet">
-    <link href="Content/css/test.css" rel="stylesheet">
+    <link href="Content/css/view_home.css" rel="stylesheet">
     <title>Page d'accueil</title>
     <script>
         function updateFormAction() {
@@ -13,20 +13,6 @@
             form.action = "?controller=home&action=recherche&recherche=" + encodeURIComponent(rechercheValue);
         }
     </script>
-    <style>
-        .feature-hover {
-    transition: background-color 0.3s ease;
-}
-
-.feature-hover:hover {
-    background-color: #your-hover-color; /* Replace with your desired hover color */
-}
-
-.feature-block:focus .feature-hover,
-.feature-block:focus-within .feature-hover {
-    background-color: #your-hover-color; /* Replace with your desired hover color */
-}
-    </style>
 </head>
 <body>
     <div id="navbar">
@@ -58,19 +44,48 @@
     </div>
     <script src="Content/js/bootstrap.bundle.min.js"></script>
     
-    <section class="my-5">
-    <h2 class="h2 mb-4">NOUVEAUTÉS</h2>
-    <div class="row row-cols-1 row-cols-md-4 g-4">
-        <!-- Repeat for each movie item -->
-        <div class="col">
-            <img src="https://placehold.co/220x330?text=Movie+Thumbnail&fontsize=25" alt="Movie thumbnail for 'Movie Title'" class="img-fluid rounded">
-        </div>
-        <div class="col">
-            <img src="https://placehold.co/220x330?text=Movie+Thumbnail&fontsize=25" alt="Movie thumbnail for 'Movie Title'" class="img-fluid rounded">
-        </div>
-        <!-- ... -->
-    </div>
+    <section class="py-8 text-white">
+        <div class="container nouveautes">
+            <hr class="border-gray-800">
+            <h2 id="fonc-title" class="text-2xl font-semibold my-5 text-center">NOUVEAUTÉS</h2>
+            <div class="row g-4 justify-content-center">
+                
+                <!-- Image frames -->
+
+                <div class="col">
+                    <div class="image-frame">
+                        <img src="https://placehold.co/220x330?text=Movie+Thumbnail&fontsize=25" alt="Movie thumbnail for 'Movie Title'" class="img-fluid rounded">
+                    </div>
+                </div>
+
+                <div class="col">
+                    <div class="image-frame">
+                        <img src="https://placehold.co/220x330?text=Movie+Thumbnail&fontsize=25" alt="Movie thumbnail for 'Movie Title'" class="img-fluid rounded">
+                    </div>
+                </div>
+
+                <div class="col">
+                    <div class="image-frame">
+                        <img src="https://placehold.co/220x330?text=Movie+Thumbnail&fontsize=25" alt="Movie thumbnail for 'Movie Title'" class="img-fluid rounded">
+                    </div>
+                </div>
+
+                <div class="col">
+                    <div class="image-frame">
+                        <img src="https://placehold.co/220x330?text=Movie+Thumbnail&fontsize=25" alt="Movie thumbnail for 'Movie Title'" class="img-fluid rounded">
+                    </div>
+                </div>
+
+                <div class="col">
+                    <div class="image-frame">
+                        <img src="https://placehold.co/220x330?text=Movie+Thumbnail&fontsize=25" alt="Movie thumbnail for 'Movie Title'" class="img-fluid rounded">
+                    </div>
+                </div>
+
+            </div>
+        </div> 
     </section>
+
 
     <section id="fonctionnalites" class="py-8 text-white">
         <div class="container">
@@ -89,7 +104,10 @@
                             <div class="fonc-box feature-content p-4">
                                 <i class="fas fa-search-plus fa-2x"></i>
                                 <h3 class="text-center text-xl font-semibold mt-2">RECHERCHES SIMPLES</h3>
-                                <p feature-text>La recherche simple permet à l'utilisateur de faire une recherche sur un film ou un acteur en tapant simplement son intitulé (nom du film, nom/prénom de l'acteur)</p>
+                                <div class="content-with-img">
+                                    <img src="Content/img/icons8-coche-100.png" class="feature-img">
+                                    <p class="feature-text-description">La recherche simple permet à l'utilisateur de faire une recherche sur un film ou un acteur en tapant simplement son intitulé (nom du film, nom/prénom de l'acteur)</p>
+                                </div>
                             </div>
                         </a>
                     </div>
@@ -100,7 +118,10 @@
                             <div class="fonc-box feature-content p-4">
                                 <i class="fas fa-cogs fa-2x"></i>
                                 <h3 class="text-center text-xl font-semibold mt-2">RECHERCHES AVANCÉES</h3>
-                                <p>La recherche avancée permet une recherche plus poussée que la recherche simple, notamment en laissant plus de filtres à l'utilisateur (genre, pays, année, etc..)</p>
+                                <div class="content-with-img">
+                                    <img src="Content/img/icons8-rafraichir-120.png" class="feature-img">
+                                    <p class="feature-text-description">La recherche avancée permet une recherche plus poussée que la recherche simple, notamment en laissant plus de filtres à l'utilisateur (genre, pays, année, etc..)</p>
+                                </div>
                             </div>
                         </a>
                     </div>
@@ -116,7 +137,10 @@
                             <div class="fonc-box feature-content p-4">
                                 <i class="fas fa-search-plus fa-2x"></i>
                                 <h3 class="text-center text-xl font-semibold mt-2">COMMUN</h3>
-                                <p feature-text>La recherche simple permet à l'utilisateur de faire une recherche sur un film ou un acteur en tapant simplement son intitulé (nom du film, nom/prénom de l'acteur)</p>
+                                <div class="content-with-img">
+                                    <img src="Content/img/icons8-dossier-ouvert-120.png" class="feature-img" >
+                                    <p class="feature-text-description">La recherche simple permet à l'utilisateur de faire une recherche sur un film ou un acteur en tapant simplement son intitulé (nom du film, nom/prénom de l'acteur)</p>
+                                </div>
                             </div>
                         </a>
                     </div>
@@ -127,7 +151,10 @@
                             <div class="fonc-box feature-content p-4">
                                 <i class="fas fa-search-plus fa-2x"></i>
                                 <h3 class="text-center text-xl font-semibold mt-2">RAPPROCHEMENT</h3>
-                                <p feature-text>La recherche "Rapprochement" permet de relier deux films ou personnes par une chaîne de personnes et films la plus courte possible suivant la relation “X a participé au film Y”</p>
+                                <div class="content-with-img">
+                                    <img src="Content/img/icons8-partager-2-100.png" class="feature-img">
+                                    <p class="feature-text-description">La recherche "Rapprochement" permet de relier deux films ou personnes par une chaîne de personnes et films la plus courte possible suivant la relation “X a participé au film Y”</p>
+                                </div>
                             </div>
                         </a>
                     </div>
